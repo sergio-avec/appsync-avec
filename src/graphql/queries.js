237a1a5256d70f9cd1c23101b310892b707504df
sergio <sfilhu@gmai.com>
2021-01-13 17:25:12 -0300

    add lambda

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,41 +1,11 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const allEstablishment = /* GraphQL */ `
-  query AllEstablishment {
-    allEstablishment {
-      id
-      name
-      description
-      location {
-        lat
-        lng
-      }
-      cep
-      fullAddress
-      neighborhood
-      city
-      state
-      slug
-    }
-  }
-`;
-export const getEstablishment = /* GraphQL */ `
-  query GetEstablishment($params: QueryGetEstablishmentParamsInput!) {
-    getEstablishment(params: $params) {
-      id
-      name
-      description
-      location {
-        lat
-        lng
-      }
-      cep
-      fullAddress
-      neighborhood
-      city
-      state
-      slug
+export const cep = /* GraphQL */ `
+  query Cep {
+    cep {
+      logradouro
+      localidade
     }
   }
 `;
