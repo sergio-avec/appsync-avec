6d8670e25ff8224f0233f128db6653d43f3ee30c
sergio <sfilhu@gmai.com>
2021-01-13 07:18:33 -0300

    Change requests

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,6 +1,25 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const allEstablishment = /* GraphQL */ `
+  query AllEstablishment {
+    allEstablishment {
+      id
+      name
+      description
+      location {
+        lat
+        lng
+      }
+      cep
+      fullAddress
+      neighborhood
+      city
+      state
+      slug
+    }
+  }
+`;
 export const getEstablishment = /* GraphQL */ `
   query GetEstablishment($params: QueryGetEstablishmentParamsInput!) {
     getEstablishment(params: $params) {
